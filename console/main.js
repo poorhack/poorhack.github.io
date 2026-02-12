f682fb969729a36075a063d8c47cfaa68e6c86fd
poorhack <wangyf2007@163.com>
2026-02-12 16:02:25 +0800

    Create main.js

---
+++ console/main.js
@@ -0,0 +1,21 @@
+function log() {
+    console.log("this is console.log message");
+}
+function warn() {
+    console.warn("this is console.warn message");
+}
+function info() {
+    console.info("this is console.info message");
+}
+function error() {
+    console.error("this is console.error message");
+}
+function debug() {
+    console.debug("this is console.debug message");
+}
+function trace() {
+    console.trace("this is console.trace message");
+}
+function table() {
+    console.table("this is console.table message");
+}
